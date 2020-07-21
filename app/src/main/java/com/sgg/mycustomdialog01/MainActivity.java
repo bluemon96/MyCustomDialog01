91982f789b5a4fc4534d1556f93197e84145d760
8luemon96 <qnrwl4fl@gmail.com>
2020-07-21 10:56:55 +0900

    Initial commit

---
+++ app/src/main/java/com/sgg/mycustomdialog01/MainActivity.java
@@ -0,0 +1,40 @@
+package com.sgg.mycustomdialog01;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements MyDialog.ExampleDialogListener {
+    private TextView textViewUsername;
+    private TextView textViewPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewUsername = (TextView) findViewById(R.id.textview_username);
+        textViewPassword = (TextView) findViewById(R.id.textview_password);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog() {
+        MyDialog exampleDialog = new MyDialog();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
+    }
+
+    @Override
+    public void applyTexts(String username, String password) {
+        textViewUsername.setText(username);
+        textViewPassword.setText(password);
+    }
+}
